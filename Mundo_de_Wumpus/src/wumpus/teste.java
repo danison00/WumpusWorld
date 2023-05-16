2ca43f5f445da04766f271b4e88103a2a21d7811
danison <danison.santos00@gmail.com>
2023-05-16 15:54:36 -0300

    -

--- Mundo_de_Wumpus/src/wumpus/teste.java
+++ Mundo_de_Wumpus/src/wumpus/teste.java
@@ -1,31 +1,13 @@
 package wumpus;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class teste {
 
-public static void main(String[] args) {
-	
-	Matriz mat = new Matriz(7);
-	
-	mat.imprimeMatriz();
-	
-	int cont=0;
-	for(int i =0; i<mat.getTamanho();i++) {
-		for(int j =0; j<mat.getTamanho();j++) {
-			
-			System.out.print(mat.matrizSensacoes.get(i).get(j)+"\t");
-			
-			cont++;
-			
-		}
-		System.out.println();
+	public static void main(String[] args) {
+
+		Matriz mat = new Matriz(7);
+
+		mat.imprimeMatriz();
+
 	}
-	
-	List<List<List<String>>> a = new ArrayList<>();
-	
-}
 
 }
