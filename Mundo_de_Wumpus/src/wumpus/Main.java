3f360cc9a8015c151294dc0b745ca206f7fdc84b
danison <danison.santos00@gmail.com>
2023-07-18 11:12:14 -0300

    final

--- Mundo_de_Wumpus/src/wumpus/Main.java
+++ Mundo_de_Wumpus/src/wumpus/Main.java
@@ -1,23 +1,19 @@
 package wumpus;
 
-import java.util.Iterator;
 
 public class Main {
 
 	public static void main(String[] args) {
 		double soma = 0;
-		for (int i = 0; i < 10; i++) {
 
-			double tempInicial = System.currentTimeMillis();
-			WumpussWorld wumpussWorld = new WumpussWorld(5);
-			wumpussWorld.run();
+		double tempInicial = System.currentTimeMillis();
+		WumpussWorld wumpussWorld = new WumpussWorld(5);
+		wumpussWorld.run();
 
-			double tempo = (System.currentTimeMillis() - tempInicial);
-				soma += tempo;
+		double tempo = (System.currentTimeMillis() - tempInicial);
+	
 
-		}
-		
-		System.out.println(soma/1000/10);
+		System.out.println(tempo / 1000 +"s");
 	}
 
 }
